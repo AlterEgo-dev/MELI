45934c4ec83df6ce64adf58bafb23c4866141d18
bruno7even <brunob2013@hotmail.com.ar>
2023-07-19 01:45:20 -0300

    añadio login+register y su responsive

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
 app.listen(puerto, () => {
     console.log(`Aplicación corriendo en puerto ${puerto}`);
 });
